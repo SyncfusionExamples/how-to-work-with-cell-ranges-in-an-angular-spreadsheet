556579f69ae6a16f825e682518047e3bc53cb415
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-01-07 09:50:53 +0530

    Angular Spreadsheet Cell Ranges video
    Angular Spreadsheet Cell Ranges video

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# how-to-work-with-cell-ranges-in-an-angular-spreadsheet
+# How to Work with Cell Ranges in an Angular Spreadsheet
+
 A quick-start project that helps you work with cell ranges in the Syncfusion Angular Spreadsheet component. This project contains code snippets to wrap text, merge two or more cells, apply data validation, and auto-fill data in a cell range.
+
+Refer to the following cell range documentation for the Syncfusion Angular Spreadsheet component:  
+https://ej2.syncfusion.com/angular/documentation/spreadsheet/cell-range 
+
+Check out this online example for data validation in the Syncfusion Angular Spreadsheet component:  
+https://ej2.syncfusion.com/angular/demos/#/material/spreadsheet/data-validation 
+
+Watch the following video to get started with the Angular Spreadsheet component: 
+https://www.youtube.com/watch?v=2Ozwe37X-7Q 
+
+Watch the following video to check data binding in the Angular Spreadsheet component: 
+https://www.youtube.com/watch?v=jCbBJdUil64 
+
+## Project prerequisites
+
+Make sure that you have the latest versions of NodeJS and Visual Studio Code in your machine before starting to work on this project.
+
+### How to run this application?
+
+To run this application, you need to clone the `how-to-work-with-cell-ranges-in-an-angular-spreadsheet` repository and then open it in Visual Studio Code. Now, simply install all the necessary react packages into your current project using the `npm install` command and run your project using the `ng serve` command.
---
+++ src/app/data.ts
@@ -0,0 +1,92 @@
+export let InventoryList: Object[] = [
+    {
+        "Item Code": "AG940Z",
+        "Item Name": "Casual Shoes",
+        "Quantity": "144",
+        "Price": "169.50",
+        "Profit": "=C2*D2",
+        "Stock Detail": "In Stock",
+        "Website": "Amazon"
+    },
+    {
+        "Item Code": "BJ120K",
+        "Item Name": "Scientific Calculator",
+        "Quantity": "116",
+        "Price": "21.80",
+        "Profit": "=C4*D4",
+        "Stock Detail": "In Stock",
+        "Website": "Overstack"
+    },
+    {
+        "Item Code": "BC120M",
+        "Item Name": "Wired Keyboard",
+        "Quantity": "438",
+        "Price": "26.80",
+        "Profit": "=C5*D5",
+        "Stock Detail": "In Stock",
+        "Website": "AliExpress"
+    },
+    {
+        "Item Code": "BS121L",
+        "Item Name": "Memory Card",
+        "Quantity": "486",
+        "Price": "23.50",
+        "Profit": "=C6*D6",
+        "Stock Detail": "Out of Stock",
+        "Website": "AliBaba"
+    },
+    {
+        "Item Code": "BU121K",
+        "Item Name": "Coffee Maker",
+        "Quantity": "176",
+        "Price": "56.50",
+        "Profit": "=C7*D7",
+        "Stock Detail": "In Stock",
+        "Website": "Taobao"
+    },
+    {
+        "Item Code": "BD121M",
+        "Item Name": "Table Lamp",
+        "Quantity": "0",
+        "Price": "22.50",
+        "Profit": "=C8*D8",
+        "Stock Detail": "Out of Stock",
+        "Website": "Amazon"
+    },
+    {
+        "Item Code": "AT992X",
+        "Item Name": "Document Scanner",
+        "Quantity": "116",
+        "Price": "175",
+        "Profit": "=C9*D9",
+        "Stock Detail": "In Stock",
+        "Website": "Overstack"
+    },
+    {
+        "Item Code": "AP992Z",
+        "Item Name": "Gaming Headset",
+        "Quantity": "58",
+        "Price": "32",
+        "Profit": "=C10*D10",
+        "Stock Detail": "Out of Stock",
+        "Website": "AliExpress"
+    },
+    {
+        "Item Code": "AW920X",
+        "Item Name": "Laptop Bag",
+        "Quantity": "232",
+        "Price": "18.90",
+        "Profit": "=C11*D11",
+        "Stock Detail": "In Stock",
+        "Website": "AliBaba"
+    },
+    {
+        "Item Code": "AQ920Z",
+        "Item Name": "Table Fan",
+        "Quantity": "405",
+        "Price": "33.90",
+        "Profit": "=C12*D12",
+        "Stock Detail": "Out of Stock",
+        "Website": "Flipkart"
+    },
+];
